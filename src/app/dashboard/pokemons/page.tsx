630fcd259fb0c7a31a5520fe2e7d0caf347fd928
AlejandroRomero17 <gonzalez.romero.alejandroo@gmail.com>
2024-05-23 15:43:19 -0500

    update

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,8 +1,16 @@
+const getPokemons = async (limit = 20, offset = 0) => {
+  const data = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+    .then(res => res.json())
 
-export default function PokemonsPage() {
+  return data
+}
+
+export default async function PokemonsPage() {
+  const pokemons = await getPokemons();
+
   return (
     <div>
-      <h1>Hello PokemonsPage</h1>
+      { JSON.stringify(pokemons)}
     </div>
   );
 }
