48cce3e42c4282cd45ccfb488a008c746f52d0a7
AlejandroRomero17 <gonzalez.romero.alejandroo@gmail.com>
2024-05-25 14:38:21 -0500

    update

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,9 +1,21 @@
+
 interface Props {
   params: { id: string };
 }
 
+const getPokemon = async (id: string) => {
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`,
+    {
+      cache: 'force-cache' //TODO: Cambiar esto en un futuro
+    }
+  ).then(response => response.json());
+  console.log(pokemon);
+}
+
 
 export default function PokemonPage({ params }: Props) {
+  const pokemon = getPokemon(params.id);
+
   return (
     <div>
       <h1>Pokemon {params.id}</h1>
