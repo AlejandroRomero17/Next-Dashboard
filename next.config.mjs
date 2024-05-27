e88c8be3a34dbae75b285a012388b63bde142c20
AlejandroRomero17 <gonzalez.romero.alejandroo@gmail.com>
2024-05-27 09:30:27 -0500

    update page pokemon

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "scontent-atl3-2.xx.fbcdn.net",
       },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com"
+      }
     ],
   },
 };
--- src/pokemons/index.ts
+++ src/pokemons/index.ts
@@ -4,3 +4,4 @@ export { PokemonGrid};
 
 export type { PokemonsResponse } from './interfaces/pokemons-response';
 export type { SimplePokemon } from './interfaces/simple-pokemon';
+export type { Pokemon } from './interfaces/pokemon';
